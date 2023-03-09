39686982554a8209640c0406ab897e24c3fa9777
icantond <cantondebat@outlook.com>
2023-03-09 19:12:52 -0300

    intento de funcionalizar formulario

--- pages/enfermeria.php
+++ pages/enfermeria.php
@@ -7,17 +7,16 @@ $codigo_area = $_POST["codigo-area"];
 $celular = $_POST["celular"];
 $mensaje = $_POST["mensaje"];
 
-$mensaje_web_enfermeria = "Este mensaje fue enviado desde el formulario de contacto de ISV/Enfermeria por " . $nombre . ",\r\n";
-$mensaje_web_enfermeria . = "Correo electrónico " .$email . "\r\n";
-$mensaje_web_enfermeria . = "Teléfono de contacto " . $codigo_area . $celular . "\r\n";
-$mensaje_web_enfermeria . = "Mensaje: " . $mensaje . "\r\n";
-$mensaje_web_enfermeria . = "Enviado el " . date(d/m/Y, time());
+$mensaje_web_enfermeria = "Este mensaje fue enviado desde el formulario de contacto de ISV/Enfermeria por $nombre \n";
+$mensaje_web_enfermeria .= "Correo electrónico $email  \n";
+$mensaje_web_enfermeria .= "Teléfono de contacto " . $codigo_area . $celular . "\r\n";
+$mensaje_web_enfermeria .= "Mensaje:  $mensaje";
+
 
 $destinatario = "m_ar23@hotmail.com";
 $asunto = "Mensaje Formulario ISV Enfermeria";
-
-mail($destinatario, $asunto, utf8_decode($mensaje_web_enfermeria), $header);
+    
+mail($destinatario, $asunto, $mensaje_web_enfermeria, $header);
 header("Location:exito.html");
 
 ?>
-
